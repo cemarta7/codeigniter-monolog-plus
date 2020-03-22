106fae80e430356151e0bac5bdcee03996cab18c
James Kachel <james@jkachel.com>
2020-03-22 14:23:32 -0500

    fixing config file stuff

--- application/config/monolog-dist.php
+++ application/config/monolog-dist.php
@@ -38,7 +38,8 @@ $cimp_config = array(
 	// Each configuration needs a few keys:
 	// 'enabled' - boolean, whether or not this particular config is enabled
 	// 'threshold' - CI threshold for logging (4 = ALL, 3 = INFO, 2 = DEBUG, 1 = ERROR)
-	//
+	// FIXME: "bubble" isn't supported quite yet
+    //
 	// These settings also affect the priority list configuration further down the file:
 	// - If the list includes a handler you don't have configured here, it will be skipped.
 	// - If the list includes a handler with a config block you've not named here, it will be skipped.
@@ -200,76 +201,3 @@ $cimp_config = array(
 
 // The following line pulls the config into the CI config namespace. Don't edit it.
 $config['cimp'] = $cimp_config;
-
-// The rest of this crap is stuff from the original version of the library and is something you should remove before
-// this gets published anywhere, James.
-
-/* GENERAL OPTIONS */
-$config['handlers'] = array('ci_file', 'loggly'); // valid handlers are ci_file | file | new_relic | hipchat | stderr | papertrail | gelf | loggly | phpconsole
-$config['introspection_processor'] = TRUE; // add some meta data such as controller and line number to log messages
-
-
-$config['channel'] = ENVIRONMENT; // channel name which appears on each line of log
-$config['threshold'] = 4; // 'ERROR' => '1', 'DEBUG' => '2',  'INFO' => '3', 'ALL' => '4'
-
-/* CI FILE - DEFAULT CODEIGNITER LOG FILE STRUCTURE
-* Log to default CI log directory (must be writable ie. chmod 757).
-* Filename will be encoded to current system date, ie. YYYY-MM-DD-ci.log
-*/
-$config['ci_file_logfile'] = APPPATH . '/logs/log.php';
-$config['ci_file_multiline'] = TRUE; //add newlines to the output
-
-/* FILE HANDLER OPTIONS
- * Log to default CI log directory (must be writable ie. chmod 757).
- * Filename will be encoded to current system date, ie. YYYY-MM-DD-ci.log
-*/
-$config['file_logfile'] = APPPATH . '/logs/ci.log';
-$config['file_multiline'] = TRUE; //add newlines to the output
-
-/* NEW RELIC OPTIONS */
-$config['new_relic_app_name'] = 'APP NAME - ' . ENVIRONMENT;
-
-/* HIPCHAT OPTIONS */
-$config['hipchat_app_token'] = ''; //HipChat API Token
-$config['hipchat_app_room_id'] = ''; //The room that should be alerted of the message (Id or Name)
-$config['hipchat_app_notification_name'] = 'Monolog'; //Name used in the "from" field
-$config['hipchat_app_notify'] = false; //Trigger a notification in clients or not
-$config['hipchat_app_loglevel'] = 'WARNING'; //The minimum logging level at which this handler will be triggered
-
-/* PAPER TRAIL OPTIONS */
-$config['papertrail_host'] = ''; //xxxx.papertrailapp.com
-$config['papertrail_port'] = ''; //port number
-$config['papertrail_multiline'] = TRUE; //add newlines to the output
-
-/* GELF OPTIONS */
-$config['gelf_host'] = ''; //xxxx.papertrailapp.com
-$config['gelf_port'] = ''; //port number
-
-/*
- * Loggly options
- * 
- * The Loggly support only has one option - 'token'. Get your customer token
- * from the setup page in Loggly and put it here. 
- *
- * Docs: https://www.loggly.com/docs/php-monolog/
- *
-*/
-$config['ci_monolog']['loggly']['token'] = ''; 
-
-/*
- * SyslogUdp options
- *
- * This is different than the Syslog handler and expects to connect to a server
- * over UDP. 
- *
-*/
-
-$config['ci_monolog']['syslogudp'] = [
-	'host' => 'localhost', // The hostname or IP to connect to.
-	'port' => 514, // The port - 514 is the default. 
-	'ident' => 'Applicaiton', // An identifier for your project. Set this to something unique so it will be easy to find in the actual log.
-	'bubble' => true // Whether messages can bubble up the stack or not (see docs for your particular syslogd server)
-];
-
-// exclusion list for pesky messages which you may wish to temporarily suppress with strpos() match
-$config['exclusion_list'] = array();
